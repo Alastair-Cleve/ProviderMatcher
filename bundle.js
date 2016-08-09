8fbe0399257c4f7e1f39ed8f81dadd566f405bdd
Alastair Cleve <AlastairCleve@Alastairs-MacBook-Pro.local>
2016-08-09 13:46:43 -0700

    Added favicon

--- bundle.js
+++ bundle.js
@@ -44,9 +44,9 @@
 /* 0 */
 /***/ function(module, exports, __webpack_require__) {
 
-	jQuery.fn.tableToCSV = __webpack_require__(2);
+	jQuery.fn.tableToCSV = __webpack_require__(1);
 
-	var providers = __webpack_require__(1);
+	var providers = __webpack_require__(2);
 	var matchRange = 1;
 	var providerKeys = Object.keys(providers);
 	var todays_date = new Date();
@@ -249,6 +249,70 @@
 
 /***/ },
 /* 1 */
+/***/ function(module, exports) {
+
+	module.exports = function() {
+	  if ($('#patient-name').val().trim() === "") {
+	    $('#patient-name').css("background-color", "lightblue");
+	    return;
+	  }
+
+	  var clean_text = function(text) {
+	    text = text.replace(/"/g, '""');
+	    return '"'+text+'"';
+	  };
+
+	  var patient = $('#patient-name').val();
+	  var comments = $('#patient-comments').val();
+		var title = [];
+		var rows = [];
+
+		$(this).find('tr').each(function() {
+			var data = [];
+
+			$(this).find('th').each(function() {
+	      var text = clean_text($(this).text());
+				title.push(text);
+			});
+
+			$(this).find('td').each(function() {
+	      var text = clean_text($(this).text());
+				data.push(text);
+			});
+
+			data = data.join(",");
+			rows.push(data);
+		});
+
+		title = title.join(",");
+		rows = rows.join("\n");
+
+		var csv = clean_text("Name:") + "," + clean_text("" + patient) + "\n" +
+	    clean_text("Date:") + "," + clean_text("" + new Date()) + "\n" +
+	    clean_text("Comments:") + "," + clean_text("" + comments) + "\n\n" + title + rows;
+		var uri = 'data:text/csv;charset=utf-8,' + encodeURIComponent(csv);
+
+	  var download_link = document.createElement('a');
+		download_link.href = uri;
+
+	  var date = new Date();
+	  var ts = date.getFullYear() + "_" + (date.getMonth() + 1) + "_" + date.getDate();
+
+		download_link.download = patient.trim().replace(/ /g,"_") + "_" + ts + ".csv";
+		document.body.appendChild(download_link);
+		download_link.click();
+		document.body.removeChild(download_link);
+
+	  $('#patient-name').val("");
+	  $('#patient-comments').val("")
+	  $('#lean_overlay').click();
+	  $('#patient-name').css("background-color", "transparent");
+
+	};
+
+
+/***/ },
+/* 2 */
 /***/ function(module, exports) {
 
 	var providers = {
@@ -747,70 +811,6 @@
 	};
 
 	module.exports = providers;
-
-
-/***/ },
-/* 2 */
-/***/ function(module, exports) {
-
-	module.exports = function() {
-	  if ($('#patient-name').val().trim() === "") {
-	    $('#patient-name').css("background-color", "lightblue");
-	    return;
-	  }
-
-	  var clean_text = function(text) {
-	    text = text.replace(/"/g, '""');
-	    return '"'+text+'"';
-	  };
-
-	  var patient = $('#patient-name').val();
-	  var comments = $('#patient-comments').val();
-		var title = [];
-		var rows = [];
-
-		$(this).find('tr').each(function() {
-			var data = [];
-
-			$(this).find('th').each(function() {
-	      var text = clean_text($(this).text());
-				title.push(text);
-			});
-
-			$(this).find('td').each(function() {
-	      var text = clean_text($(this).text());
-				data.push(text);
-			});
-
-			data = data.join(",");
-			rows.push(data);
-		});
-
-		title = title.join(",");
-		rows = rows.join("\n");
-
-		var csv = clean_text("Name:") + "," + clean_text("" + patient) + "\n" +
-	    clean_text("Date:") + "," + clean_text("" + new Date()) + "\n" +
-	    clean_text("Comments:") + "," + clean_text("" + comments) + "\n\n" + title + rows;
-		var uri = 'data:text/csv;charset=utf-8,' + encodeURIComponent(csv);
-
-	  var download_link = document.createElement('a');
-		download_link.href = uri;
-
-	  var date = new Date();
-	  var ts = date.getFullYear() + "_" + (date.getMonth() + 1) + "_" + date.getDate();
-
-		download_link.download = patient.trim().replace(/ /g,"_") + "_" + ts + ".csv";
-		document.body.appendChild(download_link);
-		download_link.click();
-		document.body.removeChild(download_link);
-
-	  $('#patient-name').val("");
-	  $('#patient-comments').val("")
-	  $('#lean_overlay').click();
-	  $('#patient-name').css("background-color", "transparent");
-
-	};
 
 
 /***/ }
